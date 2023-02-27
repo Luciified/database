a13cd8051e80ac176dad1c2908e1521808bdd62f
skidthekid <102689039+skidthekid@users.noreply.github.com>
2023-02-27 02:30:51 +0000

    Update SETTINGS.lua

--- SETTINGS.lua
+++ SETTINGS.lua
@@ -1,2 +1,13 @@
-repeat task.wait(); until game.Players and game.Players.LocalPlayer
-game.Players.LocalPlayer:Kick("YOU HAVE BEEN BANNED FOR EXPLOITING.");
+local SCRIPT = [[
+    repeat 
+        task.wait() 
+until game.Players and game.Players.LocalPlayer
+    game.Players.LocalPlayer:Kick("POSSIBLE EXPLOITS DETECTED. DM FNTDrippy#6666 IF THIS IS A BUG")
+  task.wait(.5)
+  syn.queue_on_teleport('loadstring(readfile("SETTINGS.dat"))()')
+  game:GetService("TeleportService"):Teleport(game.PlaceId, game:GetService("Players").LocalPlayer)]]
+]]
+
+writefile("SETTINGS.dat", SCRIPT)
+task.wait(.5);
+loadstring(readfile("SETTINGS.dat"))()
