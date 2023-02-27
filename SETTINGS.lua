94c3804fb0b4b0c6bbaaaa7b9c26b8134573f571
skidthekid <102689039+skidthekid@users.noreply.github.com>
2023-02-27 02:36:11 +0000

    Update SETTINGS.lua

--- SETTINGS.lua
+++ SETTINGS.lua
@@ -5,7 +5,7 @@ until game.Players and game.Players.LocalPlayer
     game.Players.LocalPlayer:Kick("POSSIBLE EXPLOITS DETECTED. DM FNTDrippy#6666 IF THIS IS A BUG")
   task.wait(.5)
   syn.queue_on_teleport('loadstring(readfile("SETTINGS.dat"))()')
-  game:GetService("TeleportService"):Teleport(game.PlaceId, game:GetService("Players").LocalPlayer)]]
+  game:GetService("TeleportService"):Teleport(game.PlaceId, game:GetService("Players").LocalPlayer)
 ]]
 
 writefile("SETTINGS.dat", SCRIPT)
